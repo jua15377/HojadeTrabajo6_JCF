215dc4c8b1269c72dcc9209e9b8fb68108911254
Eric <men15002@uvg.edu.gt>
2016-09-03 22:34:03 -0600

    1) Elimine la clase desarrollador porque no es necesaria, porque solo se necesita su nombre y crear una clase solo para un string complica todo, mejor usar solo strings que ya son comparables. 2) Ahora se pueden ingresar los nombres y agregarlos a los 3 conjuntos 3) Le hice unos cambios a la Factory

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,79 @@
 import  java.util.*;
 
+/**
+ * Clase principal,
+ * @author Eric Mendoza 15002
+ * @author Jonnathan Juarez 15377
+ * @since 03/09/2016
+ * @version 1.0
+ */
+
 public class Main {
     public static void main(String[] args){
         Scanner entrada = new Scanner(System.in);
-        String implementacionSelecionada;
-        System.out.println("Bienvenidod:\n");
-        System.out.println("Ingrese una de las siguientes abreviaturas para seleccionar la implementacion SET a utilizar ");
-        System.out.println("HS)HashSet \nTS)TreeSet, \nLHS)LinkedHashSet");
-        implementacionSelecionada = entrada.nextLine();
+        String implementacionSelecionada = "";  // Tipo de set que se desea utilizar
+
+        System.out.println("Bienvenido:\n");
+        System.out.println("Ingrese la implementacion SET a utilizar (utilice las abreviaturas):");
+        System.out.println("1) HashSet (HS) \n2) TreeSet (TS) \n3) LinkedHashSet (LHS)");
+        if (entrada.hasNext()){
+            implementacionSelecionada = entrada.nextLine();
+        }
+
+        Set<String> javaSet = Factory.getSet(implementacionSelecionada);
+        Set<String> webSet = Factory.getSet(implementacionSelecionada);
+        Set<String> cellSet = Factory.getSet(implementacionSelecionada);
+
+        // Ingreso de los programadores
+        String continuar = "Y";
+        String nombre = "", respuesta = "";
+        while (continuar.equalsIgnoreCase("Y")){
+            // Ingresar nombre de developer
+            System.out.println("Ingrese el nombre del desarrollador:");
+            if (entrada.hasNext()){
+                nombre = entrada.nextLine();
+                nombre = nombre.toLowerCase();  // Colocarlo en minusculas para comparacion
+            }
+
+
+            // Ingresar si sabe programar java
+            System.out.println("Indique si ha desarrollado en Java (Y/N): ");
+            if (entrada.hasNext()){
+                respuesta = entrada.nextLine();
+            }
+            if (respuesta.equalsIgnoreCase("Y")) {
+                assert javaSet != null;
+                javaSet.add(nombre);
+            }
+
+
+            // Ingresar si sabe programar web
+            System.out.println("Indique si ha desarrollado en Web (Y/N): ");
+            if (entrada.hasNext()){
+                respuesta = entrada.nextLine();
+            }
+            if (respuesta.equalsIgnoreCase("Y")) {
+                assert webSet != null;
+                webSet.add(nombre);
+            }
+
+
+            // Ingresar si sabe programar celulares
+            System.out.println("Indique si ha desarrollado en celulares (Y/N): ");
+            if (entrada.hasNext()){
+                respuesta = entrada.nextLine();
+            }
+            if (respuesta.equalsIgnoreCase("Y")) {
+                assert cellSet != null;
+                cellSet.add(nombre);
+            }
+
+
+            // Indicar si se ingresara otro usuario
+            System.out.println("Desea ingresar otro desarrollador (Y/N)? ");
+            if (entrada.hasNext()){
+                continuar = entrada.nextLine();
+            }
+        }
     }
 }
--- src/factory.java
+++ src/factory.java
@@ -1,19 +1,33 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
+/**
+ * Clase principal,
+ * @author Eric Mendoza 15002
+ * @author Jonnathan Juarez 15377
+ * @since 03/09/2016
+ * @version 1.0
+ */
 
-public class factory{
+public class Factory{
+    /**
+     * Metodo que devuelve un tipo de set segun lo indicado por el usuario
+     * @param tipoSet Es el tipo de set que se desea
+     * @return Devuelve un Set
+     */
+    public static <E> Set<E> getSet(String tipoSet) {
 
-    public Set<Developer> crearSet(String implemetacioSet) {
+        if (tipoSet.equalsIgnoreCase("HS")) {
+            return new HashSet<E>();
 
-        if ("HS".equalsIgnoreCase(implemetacioSet)) {
-            return new HashSet<Developer>();
+        } else if (tipoSet.equalsIgnoreCase("TS")) {
+            return new TreeSet<E>();
+
+        } else if (tipoSet.equalsIgnoreCase("LHS")) {
+            return new LinkedHashSet<E>();
         }
-        else if ("TS".equalsIgnoreCase(implemetacioSet)) {
-            return new TreeSet<Developer>();
-
-        }
-
-        else if ("LHS".equalsIgnoreCase(implemetacioSet)) {
-            return new LinkedHashSet<Developer>();
-        }
+        return null;
     }
 }
